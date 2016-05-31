b139d1200f6065e9884dab12fa161d2312747686
reniarie <reniariie@gmail.com>
2016-05-31 17:46:05 +0600

    add if d > 0

--- equation.c
+++ equation.c
@@ -9,5 +9,13 @@ int main()
 	scanf("%lf %lf %lf", &a, &b, &c);
 	d = b * b - 4 * a * c;
 	
+	if (d > 0)
+	{
+		x1 = (-b + sqrt(d)) / 2 * a;
+		x2 = (-b - sqrt(d)) / 2 * a;
+		printf("x1=%.2lf.\n", x1);
+		printf("x2=%.2lf.\n", x2);
+	}
+	
     return 0;
 }
